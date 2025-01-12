7fbc3daeeb845cfa3ec4499c2db44556a7566b8a
eberlekim <kim.eberle2002@gmail.com>
2025-01-12 22:33:35 +0100

    Add Socket.io setup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// index.js
+
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -8,15 +10,28 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3001;
 
-// Statische Dateien aus dem public-Ordner
+// Statische Dateien aus 'public' bereitstellen
 app.use(express.static('public'));
 
 // Socket.io-Logik
 io.on('connection', (socket) => {
-  console.log('Ein User ist verbunden: ', socket.id);
-  
-  socket.on('message', (text) => {
-    io.emit('message', { id: socket.id, text });
+  console.log('Ein User ist verbunden:', socket.id);
+
+  // Wenn ein Client seine Cursor-Position sendet:
+  socket.on('cursor', (data) => {
+    // data = { x, y }
+    // Wir schicken das direkt an ALLE (inkl. dem Sender) zurück
+    io.emit('cursor', {
+      id: socket.id,
+      x: data.x,
+      y: data.y
+    });
+  });
+
+  // Beim Disconnect
+  socket.on('disconnect', () => {
+    // Teilt allen mit, dass dieser Spieler weg ist
+    io.emit('playerDisconnected', socket.id);
   });
 });
 
