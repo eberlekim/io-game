7fbfa7bf9afcca402fc59fbf00f806891bd574e0
eberlekim <kim.eberle2002@gmail.com>
2025-01-12 21:43:44 +0100

    Add express server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hallo duda, from Node.js");
+// index.js
+const express = require('express');
+const app = express();
+
+// Port von Render kommt aus process.env.PORT, fallback auf 3001 (lokal)
+const PORT = process.env.PORT || 3001;
+
+// Eine Test-Route
+app.get('/', (req, res) => {
+  res.send('Hallo duda, from Node.js via Express!');
+});
+
+// Server starten
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
