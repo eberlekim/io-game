6652075fc69236388994844c94271d60db973f10
eberlekim <kim.eberle2002@gmail.com>
2025-01-13 22:17:29 +0100

    Mein kurzer Commit-Kommentar

--- server/index.js
+++ server/index.js
@@ -4,6 +4,14 @@ const { Server } = require('socket.io');
 const initGame = require('./game');
 
 const app = express();
+
+// <-- Hier die CSP per Header lockern (Debug-Zweck)
+app.use((req, res, next) => {
+  // Erlaubt Skripte von 'self' sowie 'unsafe-eval'
+  res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-eval';");
+  return next();
+});
+
 const server = http.createServer(app);
 const io = new Server(server);
 
